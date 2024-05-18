8edf177e69f453e864d0ed71457ca0e2246d32ed
Carlos Júnior <crpu4a@gmail.com>
2024-05-18 20:09:24 -0300

    data annotations

---
+++ eCommerce.Models.DataAnnotations/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eCommerce.Models
+{
+    [Index(nameof(Email), IsUnique = true, Name = "IX_Email")]
+    [Index(nameof(Nome), nameof(CPF))]
+
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        [Required]
+        [MaxLength(15)]
+        public string? Sexo { get; set; }
+        public string? RG { get; set; }
+        public string CPF { get; set; } = null!;
+        public string? NomeMae { get; set; }
+        public string? SituacaoCadastro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }
+
+        public Contato? Contato { get; set; }
+        public ICollection<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public ICollection<Departamento>? Departamentos { get; set; }
+
+        [InverseProperty("Cliente")]
+        public ICollection<Pedido>? PedidosCompradosPeloCliente {  get; set; }
+        
+        [InverseProperty("Colaborador")]
+        public ICollection<Pedido>? PedidosGerenciadosPeloColaborador { get; set; }
+        
+        [InverseProperty("Supervisor")]
+        public ICollection<Pedido>? PedidosSupervisionadosPeloColaboradorSupervisor { get; set; }
+
+    }
+}
