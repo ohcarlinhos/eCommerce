857c089216fa790530a8f22721bc8634169d5d05
Carlos Júnior <crpu4a@gmail.com>
2024-09-13 16:22:54 -0300

    tracking

---
+++ eCommerce.Console.Tracking/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using eCommerce.Console.Tracking;
+
+var db = new eCommerceContext();
+
+var usuario01 = db.Usuarios.Find(1); // EF está acompanhando.
+usuario01!.Nome = "José";
+
+db.SaveChanges();
---
+++ eCommerce.Console.Tracking/eCommerceContext.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Console.Tracking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Console.Tracking
+{
+    public class eCommerceContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<EnderecoEntrega> EnderecosEntrega { get; set; }
+        public DbSet<Contato> Contatos { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(
+                    "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce2;Integrated Security=True;")
+                .LogTo(System.Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
+            
+            
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
